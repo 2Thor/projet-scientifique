68095e9c25008431f45ae7b2081e53e10536db7a
loicBrunoCPE <loic.bruno@cpe.fr>
2020-12-17 11:42:55 +0100

    ajout securité

---
+++ IOT/SecureExchange/DataCollect.js
@@ -0,0 +1,65 @@
+//On définit un entier commun et la clé privée du DC
+let SpublicKey = 0      //clé publique Sensor
+let publicInt = 69      //entier partagé
+let DCprivateKey = 31   //clé privée DataCollect
+let DCpublicKey = publicInt * Math.exp(DCprivateKey)    //clé publique DataCollect
+let secretKey = 0       //clé secrête pour le chiffrement/déchiffrement des messages
+
+radio.setGroup(99)      //groupe radio
+ 
+//On envoi l’entier partagé et la clé publique du DataCollect jusqu’à ce qu’il ai la clé publique du sensor
+basic.forever(function () {
+    if(SpublicKey == 0) {
+        let envoi = "INIT;" + publicInt + ";" + DCpublicKey
+        radio.sendString(envoi)
+    }
+    else{
+        radio.sendString(secureMsg("coucou Sensor", secretKey, "encode"))
+    }
+})
+
+//Réception de la clé publique du sensor
+radio.onReceivedValue(function (name: string, value: number) {
+    if(name == "SensorKEY") {
+        SpublicKey = value
+	    secretKey = SpublicKey * Math.exp(DCprivateKey)
+    }
+})
+
+radio.onReceivedString(function (receivedString: string) {
+    if(secureMsg(receivedString, secretKey, "decode") == "coucou DataCollect") {
+        basic.showIcon(IconNames.Heart)
+    }
+})
+
+
+function secureMsg(str: string, key: number, mode: string): string {
+    let signe: number;
+    let codeString: number;
+    let cryptedleter: string;
+    let stringEncrypted = ""
+
+    if (mode == "encode") {
+        signe = 1
+    }
+    else{
+        sigrne = -1
+    }
+    
+    let limit = 1000
+    for (let index = 0; index < str.length; index++) {
+        key = key % (limit * signe)
+        codeString = str.charCodeAt(index)
+        cryptedleter = String.fromCharCode(codeString + key)
+        stringEncrypted = stringEncrypted + cryptedleter
+
+        if (key > 0) {
+            key = key + codeString
+        } else {
+            key = -codeString
+        }
+        
+    }
+
+    return stringEncrypted
+}
