b492a2b3d264f80e8f6967111103b7cb88fb62e4
jsbubu <jean-sebastien.bugand-bugandet@cpe.fr>
2021-01-07 13:05:19 +0100

    fin de la chaine IOT

--- Emergency/Emergency_Web_Server.py
+++ Emergency/Emergency_Web_Server.py
@@ -1,88 +1,45 @@
-import time
-import socketserver
-import threading
-import serial
-import json
+import serial, json, time
 
+#Ce script python lit les donnees recues sur ttyACM0 puis les ecrit dans un fichier
+#To do : Envoyer les donnees sur l'API Rest
+#To do : Verifier integrite des donnees
+#A lancer en sudo, a besoin de pyserial pour fonctionner (sudo pip install pyserial)
 
-HOST           = "0.0.0.0" #autorise tout le monde
-UDP_PORT       = 10000 #Port 10000salit
-MICRO_COMMANDS = ["TL" , "LT"]
-FILENAME        = "values.txt" #ecrire les valeurs reçu dans ce document
-LAST_VALUE      = "No Data" #si pas de data envoyer "no data"
 SERIALPORT = "/dev/ttyACM0"
+#SERIALPORT = "/dev/tty.usbserial-DA00G4XZ"
 BAUDRATE = 115200
 ser = serial.Serial()
 
-
-class ThreadedUDPRequestHandler(socketserver.BaseRequestHandler):
-
-    def handle(self):
-        data = self.request[0].strip()
-        data = data.decode("utf-8") #decode le message recu en byte
-        socket = self.request[1]
-        current_thread = threading.current_thread()
-        print("{}: client: {}, wrote: {}".format(current_thread.name, self.client_address, data))
-        if data != "": #si on recoit une donnée
-                print("msg recu ",data)
-
-
-class ThreadedUDPServer(socketserver.ThreadingMixIn, socketserver.UDPServer):
-    pass
-
 def initUART():
-        # ser = serial.Serial(SERIALPORT, BAUDRATE)
-        ser.port=SERIALPORT
-        ser.baudrate=BAUDRATE
-        ser.bytesize = serial.EIGHTBITS #number of bits per bytes
-        ser.parity = serial.PARITY_NONE #set parity check: no parity
-        ser.stopbits = serial.STOPBITS_ONE #number of stop bits
-        ser.timeout = None          #block read
+    # ser = serial.Serial(SERIALPORT, BAUDRATE)
+    ser.port=SERIALPORT
+    ser.baudrate=BAUDRATE
+    ser.bytesize = serial.EIGHTBITS #number of bits per bytes
+    ser.parity = serial.PARITY_NONE #set parity check: no parity
+    ser.stopbits = serial.STOPBITS_ONE #number of stop bits
+    ser.timeout = None          #block read
+    # ser.timeout = 0             #non-block read
+    # ser.timeout = 2              #timeout block read
+    ser.xonxoff = False     #disable software flow control
+    ser.rtscts = False     #disable hardware (RTS/CTS) flow control
+    ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
+    #ser.writeTimeout = 0     #timeout for write
+    print ("Starting Up Serial Monitor")
+    try:
+        ser.open()
+    except serial.SerialException:
+        print("Serial {} port not available".format(SERIALPORT))
+        exit()        
 
-        # ser.timeout = 0             #non-block read
-        # ser.timeout = 2              #timeout block read
-        ser.xonxoff = False     #disable software flow control
-        ser.rtscts = False     #disable hardware (RTS/CTS) flow control
-        ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
-        #ser.writeTimeout = 0     #timeout for write
-        print ("Starting Up Serial Monitor")
-        try:
-                ser.open()
-                while True:
-                        print ("Je lis sur la liaison serie")
-                        time.sleep(5)
-                ser.close()
-        except serial.SerialException:
-                print("Serial {} port not available".format(SERIALPORT))
-                exit()
+initUART()
 
-
-
-
-
-
-# Main program logic follows:
-if __name__ == '__main__':
-        print ('Press Ctrl-C to quit.')
-        initUART()
+while ser.isOpen() : 
+    time.sleep(1)
+    if (ser.inWaiting() > 0): # if incoming bytes are waiting 
+        f= open("uart.log","a") #ouverture du fichier
+        data_str = ser.read(ser.inWaiting()).decode("utf-8")
+        f.write(data_str + "\n") #ecriture dans le fichier
+        print(data_str) #ecriture dans la console, permet de vérifier la donnée reçu
 
 
-        server = ThreadedUDPServer((HOST, UDP_PORT), ThreadedUDPRequestHandler)
-
-        server_thread = threading.Thread(target=server.serve_forever)
-        server_thread.daemon = True
-
-        try:
-                server_thread.start()
-                print("Server started at {} port {}".format(HOST, UDP_PORT))
-                while True :
-                        pass
-
-                
-                                
-        except (KeyboardInterrupt, SystemExit):
-                server.shutdown()
-                server.server_close()
-                print ('Serv ferme')
-                exit()
-
+        
