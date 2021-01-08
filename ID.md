acbf951f15ea1974d8ded003bf5ae03eb55ad78f
LoicBrunoCPE <loic.bruno@cpe.fr>
2021-01-08 14:16:47 +0100

    fichier id

--- ID.md
+++ ID.md
@@ -1,6 +1,18 @@
 # projet-scientifique
 
+## Mosquitto
+IP: 1.1.1.1
+Port: 1883
+
+## Emergency
+IP: 1.1.1.20
+
+## Cloud
+IP: 1.1.1.10
+
 ## InfluxDB
 Nom de la BDD: Gestion_Feu
 Utilisateur: mqtt
 MdP: N0tC0mpl1c4t3d
+
+## Graphana
--- test/test_mqtt.py
+++ test/test_mqtt.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_mqtt import Mqtt
 
 app = Flask(__name__)
-app.config['MQTT_BROKER_URL'] = '1.1.1.20'
+app.config['MQTT_BROKER_URL'] = '1.1.1.1'
 app.config['MQTT_BROKER_PORT'] = 1883
 
 mqtt = Mqtt(app)
@@ -15,4 +15,3 @@ def handle_connect(client, userdata, flags, rc):
 
 print('Publishing...')
 mqtt.publish('Emergency/test', 'ca marche !!')
-print('Published')
