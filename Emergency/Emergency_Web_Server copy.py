8deeaa88e7f84003bf43a31653daf604b8c936ea
jsbubu <jean-sebastien.bugand-bugandet@cpe.fr>
2021-01-12 17:07:35 +0100

    séparation web et uart

---
+++ Emergency/Emergency_Web_Server copy.py
@@ -0,0 +1,40 @@
+import paho.mqtt.client as mqtt
+
+app = Flask(__name__)
+
+@app.route('/')
+def map_func():
+	return render_template('map.html')
+    
+MQTT_ADDRESS = '1.1.1.1'
+MQTT_USER = 'mqtt'
+MQTT_PASSWORD = 'n0t3sy'
+MQTT_TOPIC = 'Emergency/test'
+MQTT_CLIENT_ID = 'MQTTInfluxDBBridge'
+
+def on_connect(client, userdata, flags, rc):
+    #Callback when receives a CONNACK response
+    print('Connected, returned code :' + str(rc))
+    client.subscribe(MQTT_TOPIC)
+    
+# Main program logic follows:
+if __name__ == '__main__':
+
+    mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
+    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.connect(MQTT_ADDRESS, 1883)
+
+    print ('Press Ctrl-C to quit.')
+    try:
+        print('Publishing...')
+        mqtt_client.publish(MQTT_TOPIC, '{ x: "1",y: "2",i: "7" }') 
+
+    except (KeyboardInterrupt, SystemExit):
+        ser.close()
+        exit()
+
+
+ 
+
+        
