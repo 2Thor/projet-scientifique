ca17d6e3c0590ec9efddd0b2da8c88966fe16981
clement <clement.dethoor@yahoo.fr>
2021-01-14 00:00:39 +0100

    accepted

--- serveur_api/apiRouter.js
+++ serveur_api/apiRouter.js
@@ -13,17 +13,16 @@ exports.router = (function() {
     //apiRouter.route('/users/login/').post(usersCtrl.login);
 
     //Routes real_data
-<<<<<<< HEAD
+
     apiRouter.route('/real_data/create_feu').post(real_dataCtrl.create_feu);
     apiRouter.route('/real_data/get_feu/').get(real_dataCtrl.get_feu);
     apiRouter.route('/real_data/affectation_vehicule').post(real_dataCtrl.affectation_vehicule);
     apiRouter.route('/real_data/get_vehicule').get(real_dataCtrl.get_vehicule);
-    apiRouter.route('/real_data/get_vehicule').get(real_dataCtrl.vehicule_dispo);
-    apiRouter.route('/real_data/get_vehicule').get(real_dataCtrl.vehicule_non_dispo);
-=======
+    apiRouter.route('/real_data/vehicule_dispo').get(real_dataCtrl.vehicule_dispo);
+    apiRouter.route('/real_data/vehicule_non_dispo').get(real_dataCtrl.vehicule_non_dispo);
     apiRouter.route('/real_data/feu').post(real_dataCtrl.create_feu);
     apiRouter.route('/real_data/get_feu').get(real_dataCtrl.get_feu);
->>>>>>> 5c572fcd2786a2e7531598436e5305b7f61df7ab
+
 
 
     //Routes simulate_data
