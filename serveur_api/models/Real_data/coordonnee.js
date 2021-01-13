d7776193f3a473d6b582fdc0c33e695be8cbc95c
Dethoor Clément <clement.dethoor@cpe.fr>
2021-01-13 15:28:24 +0100

    Update API databases

--- serveur_api/models/Real_data/coordonnee.js
+++ serveur_api/models/Real_data/coordonnee.js
@@ -10,9 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.Coordonnee.hasMany(models.Caserne);
-      models.Coordonnee.hasMany(models.Feu_reel);
-      models.Coordonnee.hasMany(models.Vehicule);
+      // define association here
     }
   };
   Coordonnee.init({
